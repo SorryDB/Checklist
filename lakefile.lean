0227cef80ff655abfec8040c188b6515c8ee2182
LennyTaelman <lenny.taelman@gmail.com>
2025-06-18 14:50:45 +0200

    chore: bump mathlib version

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ package «Checklist» where
   -- add any additional package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.15.0"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.20.1"
 
 @[default_target]
 lean_lib «Checklist» where
